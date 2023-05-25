00c4c33fb8659cb76516738eac8524e2d7b572f4
hoangha642003 <129708029+hoangha642003@users.noreply.github.com>
2023-05-25 23:36:26 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-dsg7qt
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-dsg7qt)
---
+++ script.js
@@ -0,0 +1,80 @@
+//bai9
+//function flipArray(numberArr){
+  //let arrTemo=[];
+  //for(let index = numberArr.length-1;index>=0;index--){
+   // let item=numberArr[index]
+    //arrTemo.push(item);
+  //}
+  //return arrTemo;
+
+//}
+//let arr=[1,2,3,4]
+//let arrFlip=flipArray(arr)
+//console.log(arrFlip)
+
+//bai8
+// function numbercheckzero(number){
+//   let isbigger=Number(number)>0
+//   return isbigger
+// }
+// let numbercheck=2
+// let big=numbercheckzero(numbercheck)
+// console.log(big)
+
+//bai5
+//function factorial(x) 
+//{ 
+
+  //if (x === 0)
+ //{
+   // return 1;
+ //}
+  //return x * factorial(x-1);
+         
+//}
+//let check=5
+// result=factorial(check)
+//console.log(result)
+
+//bai3
+//function factorial(x) 
+//{ 
+
+  //if (x === 0)
+ //{
+   // return 1;
+ //}
+  //return x * x;
+         
+//}
+//let check=125
+//let result=factorial(check)
+//console.log(result)
+
+//bai4
+// function perimeterRound(r)
+// { 
+
+//   if (r === 0)
+//  {
+//     return 1;
+//  }
+//   return r*2*3.14
+  
+         
+// }
+// let check=3
+// let result=perimeterRound(check)
+// console.log(result)
+//bai6
+// function numbercheckZero(number){
+//   let isbigger  =Number!=(number)
+//   return isbigger
+// }
+
+
+// let numbercheck=1
+// let big=numbercheckZero(numbercheck)
+// console.log(big)
+
+
